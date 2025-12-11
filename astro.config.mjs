85c16523cbae5424f486115ba563afab2d2c8b9e
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-12-11 13:58:59 -0300

    alteracao assto.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // Adicione isso se o seu URL for dmm76.github.io
-  base: "/paradigmas_programacao",
+  base: "/paradigmas_programacao/dist",
 });
